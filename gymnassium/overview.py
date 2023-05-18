78420271d5966b7a20486c0960c36d5b766f1bea
asuzukosi <keloasuzu@yahoo.com>
2023-05-18 01:02:52 +0100

    feat: made changes to overview

--- gymnassium/overview.py
+++ gymnassium/overview.py
@@ -4,14 +4,13 @@ import panda_gym
 import time
 
 # setup the environment and the observation and info
-env = gym.make("PandaStack-v3", render_mode="human", render_width=200,render_height=200,)
+env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset(seed=42)
 
 print("The action space is ", env.action_space)
 
 for _ in range(10000):
     action = env.action_space.sample() # the policy to specify the action will be placed here
-    time.sleep(0.5)
     observation, reward, terminated, truncated, info = env.step(action)
     
     if terminated or truncated:
